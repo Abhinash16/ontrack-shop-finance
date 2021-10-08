8f33933208dd2928208586971ff4f7dd5e91249f
Abhinash Shaema <abhinash16595@gmail.com>
2021-10-08 15:54:33 +0530

    'finalComponentCheck'

--- src/modules/finance/views/personalDetails.vue
+++ src/modules/finance/views/personalDetails.vue
@@ -1,24 +1,81 @@
 <template>
   <v-container>
-    <p>This is personal Details Page</p>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-text-field
+        v-model="name"
+        :counter="10"
+        :rules="nameRules"
+        label="Name"
+        required
+      ></v-text-field>
+
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+        required
+      ></v-text-field>
+
+      <v-select
+        v-model="select"
+        :items="items"
+        :rules="[(v) => !!v || 'Item is required']"
+        label="Item"
+        required
+      ></v-select>
+
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[(v) => !!v || 'You must agree to continue!']"
+        label="Do you agree?"
+        required
+      ></v-checkbox>
+
+      <!-- <v-btn :disabled="!valid" color="success" class="mr-4" @click="validate">
+        Validate
+      </v-btn> -->
+      <v-row class="my-5" align="center" justify="space-around">
+        <v-btn tile color="primary" @click="validate" depressed>
+          Continue <v-icon class="my-0 py-0">mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-row>
+    </v-form>
   </v-container>
 </template>
 
 <script>
-import axios from "axios";
 export default {
-  async created() {
-    this.isLoading = true;
+  data: () => ({
+    valid: true,
+    name: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => (v && v.length <= 10) || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    select: null,
+    items: ["Item 1", "Item 2", "Item 3", "Item 4"],
+    checkbox: false,
+  }),
 
-    try {
-      const { data } = await axios.get(
-        `https://fakestoreapi.com/products/${this.$route.params.id}`
-      );
-      this.product = data;
-    } finally {
-      this.isLoading = false;
-    }
-  }
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+      setTimeout(() => {
+        this.resetValidation();
+      }, 1000);
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
 };
 </script>
 
