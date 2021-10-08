f01d4702e7a0eca6988a3cbef3771e5a11826fc4
Abhinash Shaema <abhinash16595@gmail.com>
2021-10-08 16:00:03 +0530

    'finalComponentCheck'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="home">
-    <h1>
-      Home Page
-    </h1>
+    <h1>Home Page</h1>
 
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum aliquid
@@ -10,11 +8,14 @@
       Molestias exercitationem quod ut aspernatur nostrum debitis molestiae
       ipsam, dolores assumenda alias quidem.
     </p>
+    <v-btn tile color="primary" :to="`/finance`" depressed>
+      Jump to finance page <v-icon class="my-0 py-0">mdi-chevron-right</v-icon>
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
 };
 </script>
